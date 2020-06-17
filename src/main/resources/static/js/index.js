dfb970c6814373342218b44a7ccf878f3f723661
itchenyumeng <2390651924@qq.com>
2020-06-17 10:13:20 +0800

    更新首页

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -5,7 +5,8 @@ let app = new Vue({
         defaultActive: '首页',
         data: {},
         time: null,
-
+        MycatFirewallConfig: [],
+        blackList: [],
     },
     created() {
         this.init(); //初始化
@@ -24,6 +25,52 @@ let app = new Vue({
             this.$http.get(api.mycat.properties.schema.getMycatSchemaConfig).then(res => {
 
             });
+            this.getFirewall();
+        },
+        getFirewall() {
+            this.$http.get(api.mycat.properties.server.getMycatAllFirewallConfig).then(response => {
+                let whiteHost = this.objToArr(response.body.data.whitehost);
+                let whiteHostMask = this.replaceIP(this.objToArr(response.body.data.whitehostMask));
+                whiteHost.forEach(value => {
+                    this.MycatFirewallConfig.push(value);
+                });
+                whiteHostMask.forEach(value => {
+                    this.MycatFirewallConfig.push(value);
+                });
+                this.blackList = this.objToArr(response.body.data.blacklist);
+            });
+        },
+        replaceIP(whiteHostMask, index) {
+            if (index == 1) {
+                whiteHostMask.key = whiteHostMask.key.replace(/\*/g, '[0-9]*').replace(/\./g, '\\\.');
+                return whiteHostMask;
+            }
+            whiteHostMask.forEach(value => {
+                value.key = value.key.replace(/\[0-9\]\*/g, '*').replace(/\\\./g, '.');
+            });
+            return whiteHostMask;
+        },
+        objToArr(obj) {
+            let entries = Object.entries(obj);
+            let arr = [];
+            for (let i = 0; i < entries.length; i++) {
+                if (entries[i] != undefined) {
+                    let names = [];
+                    let item = entries[i][1];
+                    if (item instanceof Array) {
+                        item.forEach(value => {
+                            names.push(value.name);
+                        });
+                    }
+                    arr.push({
+                        key: entries[i][0],
+                        value: item,
+                        names: names,
+                        index: i
+                    });
+                }
+            }
+            return arr;
         },
         getTime(start, now) {
             if (start != undefined && now != undefined) {
